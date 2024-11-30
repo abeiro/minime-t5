182f5b26654d1dbc7f706fd8b6ec8836985cd11a
Augusto Beiro <augusto.beiro@gmail.com>
2024-11-30 12:07:36 +0100

    * Added support for oghma

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import time
 app = FastAPI()
 
 # Check if GPU is available
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print(f"Using device: {device}")
 if device.type == 'cuda':
     print(f"Device name: {torch.cuda.get_device_name(0)}")
     print(f"CUDA version: {torch.version.cuda}")
-    
+   
     
 # Load the T5 tokenizer and model from the Hugging Face model hub
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small", legacy=False, clean_up_tokenization_spaces=True)
@@ -80,6 +81,22 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
         }
     
    
+    input_text = f"\"{text} (talking to a friend)\"\nis the above sentence an intent to give gold?\nAnswer with either 'yes' or 'no'"
+    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+        
+    if "yes" in trigger_command:
+        return {
+            "input_text": text,
+            "is_command": "TakeGoldFromPlayer (amount on target property)",
+            "elapsed_time": f"{elapsed_time:.2f} seconds"
+        }
+    
+    
     # Step 2: OpenInventory
     input_text = f"\"{text}\"\nis the above sentence an explicit order to attack?\nAnswer with either 'yes' or 'no'"
     input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
@@ -96,11 +113,31 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
             "elapsed_time": f"{elapsed_time:.2f} seconds"
         }
     
+   
+
+    # Step 2: OpenInventory
+    input_text = f"\"{text}\"\nis the main verb move or come?\nAnswer with either 'yes' or 'no'"
+    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+        
+    if "yes" in trigger_command:
+        return {
+            "input_text": text,
+            "response":trigger_command,
+            "is_command": "FollowPlayer or LeadTheWayTo",
+            "elapsed_time": f"{elapsed_time:.2f} seconds"
+        }
+    
     return {
         "input_text": text,
         "is_command": "Talk",
         "elapsed_time": f"{elapsed_time:.2f} seconds"
     }
+
 
 @app.get("/extract")
 def extract_info(text: str = Query(..., description="Text to process for keyword extraction")):
@@ -130,6 +167,11 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
     end_time = time.time()
     elapsed_time = end_time - start_time
     
+    input_text = f"\"{text}\"\nExtract sailent words from above paragraph"
+    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    trigger_memory = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
     return {
         "input_text": text,
         "generated_tags": "",
@@ -143,7 +185,7 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
     start_time = time.time()
     
     # Step 1: Extract Keywords
-    input_text = f"What are the keywords in the following sentence:\n\n{text}"
+    input_text = f"What is the subject of the following sentence:\n\n{text}"
     input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
     outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
     generated_tags = tokenizer.decode(outputs[0], skip_special_tokens=True)
