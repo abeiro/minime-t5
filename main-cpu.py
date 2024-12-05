2397777680a2ccd35dc195ef97881881e598a795
Augusto Beiro <augusto.beiro@gmail.com>
2024-12-05 22:59:17 +0100

    * Added the call

--- main-cpu.py
+++ main-cpu.py
@@ -12,10 +12,6 @@ app = FastAPI()
 device = torch.device('cpu')
 
 print(f"Using device: {device}")
-if device.type == 'cuda':
-    print(f"Device name: {torch.cuda.get_device_name(0)}")
-    print(f"CUDA version: {torch.version.cuda}")
-   
     
 # Load the T5 tokenizer and model from the Hugging Face model hub
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small", legacy=False, clean_up_tokenization_spaces=True)
@@ -76,25 +72,26 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
     if "yes" in trigger_command:
         return {
             "input_text": text,
+            # "is_command": "ExchangeItems or GiveItemToPlayer or TakeItemsFromPlayer",
             "is_command": "ExchangeItems",
             "elapsed_time": f"{elapsed_time:.2f} seconds"
         }
     
    
-    input_text = f"\"{text} (talking to a friend)\"\nis the above sentence an intent to give gold?\nAnswer with either 'yes' or 'no'"
-    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
-    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
-    trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # input_text = f"\"{text} (talking to a friend)\"\nis the above sentence an intent to give gold?\nAnswer with either 'yes' or 'no'"
+    # input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    # outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    # trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    # end_time = time.time()
+    # elapsed_time = end_time - start_time
         
-    if "yes" in trigger_command:
-        return {
-            "input_text": text,
-            "is_command": "TakeGoldFromPlayer (amount on target property)",
-            "elapsed_time": f"{elapsed_time:.2f} seconds"
-        }
+    # if "yes" in trigger_command:
+    #     return {
+    #         "input_text": text,
+    #         "is_command": "TakeGoldFromPlayer (amount on target property)",
+    #         "elapsed_time": f"{elapsed_time:.2f} seconds"
+    #     }
     
     
     # Step 2: OpenInventory
@@ -116,27 +113,27 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
    
 
     # Step 2: OpenInventory
-    input_text = f"\"{text}\"\nis the main verb move or come?\nAnswer with either 'yes' or 'no'"
-    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
-    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
-    trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # input_text = f"\"{text}\"\nis the main verb move or come?\nAnswer with either 'yes' or 'no'"
+    # input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    # outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    # trigger_command = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    # end_time = time.time()
+    # elapsed_time = end_time - start_time
         
-    if "yes" in trigger_command:
-        return {
-            "input_text": text,
-            "response":trigger_command,
-            "is_command": "FollowPlayer or LeadTheWayTo",
-            "elapsed_time": f"{elapsed_time:.2f} seconds"
-        }
-    
-    return {
-        "input_text": text,
-        "is_command": "Talk",
-        "elapsed_time": f"{elapsed_time:.2f} seconds"
-    }
+    # if "yes" in trigger_command:
+    #     return {
+    #         "input_text": text,
+    #         "response":trigger_command,
+    #         "is_command": "FollowPlayer or LeadTheWayTo",
+    #         "elapsed_time": f"{elapsed_time:.2f} seconds"
+    #     }
+    # 
+    # return {
+    #     "input_text": text,
+    #     "is_command": "Talk",
+    #     "elapsed_time": f"{elapsed_time:.2f} seconds"
+    # }
 
 
 @app.get("/extract")
@@ -185,14 +182,25 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
     start_time = time.time()
     
     # Step 1: Extract Keywords
+    #input_text = f"samsum: Dialogue:\n{text}\nWhat were the main points in that conversation? "
+    #input_text = f"Here is a dialogue:\n{text}\n\nWrite a short summary!"
+
+    #input_text = f"samsum: Here is a dialogue:\n{text}\n\nWhat were they talking about?"
+
+    #input_text = f"ag_news_subset: Write a short title:\n{text} "
+    #input_text = f"newsroom:Here is an article:\n\n{text}\n\nWrite a title for it."
+    #input_text = f"Identify the most salient words in this sentence:\n\n{text}"
     input_text = f"What is the subject of the following sentence:\n\n{text}"
+
+    print(input_text)
+
     input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
-    outputs = model.generate(input_ids, max_length=50, num_beams=5, early_stopping=True)
+    outputs = model.generate(input_ids, max_length=25, num_beams=5, early_stopping=True)
     generated_tags = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
     end_time = time.time()
     elapsed_time = end_time - start_time
-    
+    print(generated_tags)
     return {
         "input_text": text,
         "generated_tags": generated_tags,
@@ -234,6 +242,24 @@ def extract_info(text: str = Query(..., description="Text to process for keyword
         "elapsed_time": f"{elapsed_time:.2f} seconds"
     }
 
-# To run the FastAPI app, save the script as `main.py` and run the following command:
-# uvicorn main:app --reload
+@app.get("/posttopic")
+def extract_info(text: str = Query(..., description="Text to process for keyword extraction")):
+    start_time = time.time()
+    
+    # Step 1: Extract Keywords
+    input_text = f"What is the subject of the following sentence:\n\n{text}"
+ 
+    print(input_text)
 
+    input_ids = tokenizer.encode(input_text, return_tensors='pt', truncation=True).to(device)
+    outputs = model.generate(input_ids, max_length=25, num_beams=5, early_stopping=True)
+    generated_tags = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(generated_tags)
+    return {
+        "input_text": text,
+        "generated_tags": generated_tags,
+        "elapsed_time": f"{elapsed_time:.2f} seconds"
+    }
